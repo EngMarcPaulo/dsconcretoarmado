f2ff5dca02cbe2e352703c983fde42860846c897
Marcelo Paulo da Silva <mrcvcoln@gmail.com>
2024-12-06 21:58:57 -0300

    Project Created

---
+++ src/main/java/com/engenharia/gessist/dsconcretoarmado/DsconcretoarmadoApplication.java
@@ -0,0 +1,13 @@
+package com.engenharia.gessist.dsconcretoarmado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsconcretoarmadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsconcretoarmadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engenharia/gessist/dsconcretoarmado/DsconcretoarmadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engenharia.gessist.dsconcretoarmado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsconcretoarmadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
